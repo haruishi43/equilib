a5897d06296f1bed7ee6393caa5a2509c5d5f2a4
Toraudonn <toraudonn@gmail.com>
2019-04-05 20:12:14 +0900

    update

--- pano2pers.py
+++ pano2pers.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import cv2
+from scipy import ndimage
 import numpy as np
 
 
@@ -14,15 +15,10 @@ class Pano2Pers:
         self.pers_img = np.zeros(self.pers_shape)
         print(f'Perspective Image: w={self.pers_w}, h={self.pers_h}')
 
-        self.pano_w = 4*w
-        self.pano_h = 4*h
-        self.pano_shape = (self.pano_w, self.pano_h)
-        self.pano_img = np.zeros(self.pano_shape)
-        print(f'Panoramic Image: w={self.pano_w}, h={self.pano_h}')
-
-        K = self.create_intrinsic_param(w, h, fov)
-        self.K_inv = np.linalg.inv(K)
-        self.im2ori = np.zeros((3,3))
+        self.radius = 1
+        self.fov_x = fov
+        self.rot_1 = 0
+        self.rot_2 = 0
 
         self.cuda = False
 
@@ -43,99 +39,66 @@ class Pano2Pers:
     
     # supporting function
 
-    def create_intrinsic_param(self, w, h, fov):
-        fov_rad = fov * np.pi / 180.0
-        f = w/(2.0 * np.tan(fov_rad / 2.0))
-        _K = [[f, 0., w/2], [0., f, h/2], [0., 0., 1.]]
-        return np.array(_K)
-
     def set_rotation(self, angles):
-        # default [yaw, pitch, roll]
-        rot = self.angles2rot(np.array(angles))
-        self.im2ori = np.dot(np.linalg.inv(rot), self.K_inv)
+        # [yaw, pitch, roll]
+        theta = angles[0]
+        phi = angles[0]
 
-    def angles2rot(self, angles):
-        """
-        FIXME: the code works, but this calculation might be hacky
-        """
-        A = np.array([
-            [1., 0., 0.],
-            [0., np.cos(angles[1]), -np.sin(angles[1])],
-            [0., np.sin(angles[1]), np.cos(angles[1])]
-        ])
-        B = np.array([
-            [np.cos(angles[0]), 0., -np.sin(angles[0])],
-            [0., 1., 0.],
-            [np.sin(angles[0]), 0., np.cos(angles[0])]
-        ])
-        C = np.array([
-            [np.cos(angles[2]), np.sin(angles[2]), 0.],
-            [-np.sin(angles[2]), np.cos(angles[2]), 0.],
-            [0., 0., 1.]
-        ])
-        return np.linalg.multi_dot([A, B, C])
+        y_axis = np.array([0.0, 1.0, 0.0], np.float32)
+        z_axis = np.array([0.0, 0.0, 1.0], np.float32)
+        [rot_1, _] = cv2.Rodrigues(z_axis * np.radians(theta))
+        [rot_2, _] = cv2.Rodrigues(np.dot(rot_1, y_axis) * np.radians(phi))
+
+        self.rot_1 = rot_1
+        self.rot_2 = rot_2
 
     def cuda(self):
         self.cuda = True
 
-    def get_perspective(self, pano_img, radius=128):
+    def get_perspective(self, pano_img):
         """
         pano: numpy array
         """
 
-        pers_img = self._with_numpy(pano_img, radius)
-
-
-
-
-        return pers_img
-
-
-    def _with_numpy(self, pano_img, radius=128):
-        FOV = 90
-        THETA = 0
-        PHI = 0
-        RADIUS = radius
-        height = self.pers_img.shape[1]
-        width = self.pers_img.shape[0]
-
-        equ_h = self.pano_img.shape[1]
-        equ_w = self.pano_img.shape[0]
-        equ_cx = (equ_w - 1) / 2.0
-        equ_cy = (equ_h - 1) / 2.0
-
-        wFOV = FOV
-        hFOV = float(height) / width * wFOV
-
-        c_x = (width - 1) / 2.0
-        c_y = (height - 1) / 2.0
-
-        wangle = (180 - wFOV) / 2.0
-        w_len = 2 * RADIUS * np.sin(np.radians(wFOV / 2.0)) / np.sin(np.radians(wangle))
-        w_interval = w_len / (width - 1)
-
-        hangle = (180 - hFOV) / 2.0
-        h_len = 2 * RADIUS * np.sin(np.radians(hFOV / 2.0)) / np.sin(np.radians(hangle))
-        h_interval = h_len / (height - 1)
-        x_map = np.zeros([height, width], np.float32) + RADIUS
-        y_map = np.tile((np.arange(0, width) - c_x) * w_interval, [height, 1])
-        z_map = -np.tile((np.arange(0, height) - c_y) * h_interval, [width, 1]).T
-        D = np.sqrt(x_map**2 + y_map**2 + z_map**2)
-        xyz = np.zeros([height, width, 3], np.float)
-        xyz[:, :, 0] = (RADIUS / D * x_map)[:, :]
-        xyz[:, :, 1] = (RADIUS / D * y_map)[:, :]
-        xyz[:, :, 2] = (RADIUS / D * z_map)[:, :]
+        R = self.radius
+        rot_1 = self.rot_1
+        rot_2 = self.rot_2
+        pers_h = self.pers_img.shape[1]
+        pers_w = self.pers_img.shape[0]
+        pers_cx = (pers_w - 1) / 2.0
+        pers_cy = (pers_h - 1) / 2.0
+        pano_h = pano_img.shape[0]
+        pano_w = pano_img.shape[1]
+        pano_cx = (pano_w - 1) / 2.0
+        pano_cy = (pano_h - 1) / 2.0
         
-        y_axis = np.array([0.0, 1.0, 0.0], np.float32)
-        z_axis = np.array([0.0, 0.0, 1.0], np.float32)
-        [R1, _] = cv2.Rodrigues(z_axis * np.radians(THETA))
-        [R2, _] = cv2.Rodrigues(np.dot(R1, y_axis) * np.radians(-PHI))
+        fov_x = self.fov_x
+        fov_y = float(pers_h) / pers_w * self.fov_x
 
-        xyz = xyz.reshape([height * width, 3]).T
-        xyz = np.dot(R1, xyz)
-        xyz = np.dot(R2, xyz).T
-        lat = np.arcsin(xyz[:, 2] / RADIUS)
-        lon = np.zeros([height * width], np.float)
+        wangle = (180 - fov_x) / 2.0
+        w_len = 2 * R * np.sin(np.radians(fov_x / 2.0)) / np.sin(np.radians(wangle))
+        w_interval = w_len / (pers_w - 1)
+
+        hangle = (180 - fov_y) / 2.0
+        h_len = 2 * R * np.sin(np.radians(fov_y / 2.0)) / np.sin(np.radians(hangle))
+        h_interval = h_len / (pers_h - 1)
+
+        x_map = np.zeros([pers_h, pers_w], np.float32) + R
+        y_map = np.tile((np.arange(0, pers_w) - pers_cx) * w_interval, [pers_h, 1])
+        z_map = -np.tile((np.arange(0, pers_h) - pers_cy) * h_interval, [pers_w, 1]).T
+        D = np.sqrt(x_map**2 + y_map**2 + z_map**2)
+
+        xyz = np.zeros([pers_h, pers_w, 3], np.float)
+        xyz[:, :, 0] = (R / D * x_map)[:, :]
+        xyz[:, :, 1] = (R / D * y_map)[:, :]
+        xyz[:, :, 2] = (R / D * z_map)[:, :]
+        
+        xyz = xyz.reshape([pers_h * pers_w, 3]).T
+        xyz = np.dot(rot_1, xyz)
+        xyz = np.dot(rot_2, xyz).T
+
+        lat = np.arcsin(xyz[:, 2] / R)
+        lon = np.zeros([pers_h * pers_w], np.float)
         theta = np.arctan(xyz[:, 1] / xyz[:, 0])
         idx1 = xyz[:, 0] > 0
         idx2 = xyz[:, 1] > 0
@@ -147,15 +110,22 @@ class Pano2Pers:
         lon[idx3] = theta[idx3] + np.pi
         lon[idx4] = theta[idx4] - np.pi
 
-        lon = lon.reshape([height, width]) / np.pi * 180
-        lat = -lat.reshape([height, width]) / np.pi * 180
-        lon = lon / 180 * equ_cx + equ_cx
-        lat = lat / 90 * equ_cy + equ_cy
-    
+        lon = lon.reshape([pers_h, pers_w]) / np.pi * 180
+        lat = -lat.reshape([pers_h, pers_w]) / np.pi * 180
+        lon = lon / 180 * pano_cx + pano_cx
+        lat = lat / 90 * pano_cy + pano_cy
+
+        _map = np.stack([lat, lon], -1)
+        _map = np.rollaxis(_map, -1)
+        print(_map.shape)
+        print(pano_img.shape)
+
         persp = cv2.remap(pano_img, lon.astype(np.float32), lat.astype(np.float32), cv2.INTER_CUBIC, borderMode=cv2.BORDER_WRAP)
+        # pers_img = np.zeros([pers_h, pers_w, 3])
+        # pers_img[:,:,0] = ndimage.map_coordinates(pano_img[:,:,0], _map.astype(np.float32), order=1, mode='wrap')
+        # pers_img[:,:,1] = ndimage.map_coordinates(pano_img[:,:,1], _map.astype(np.float32), order=1, mode='wrap')
+        # pers_img[:,:,2] = ndimage.map_coordinates(pano_img[:,:,2], _map.astype(np.float32), order=1, mode='wrap')
+
+        # print(pers_img.shape)
+
         return persp
-
-
-    def _with_gpu(self):
-
-        pass
