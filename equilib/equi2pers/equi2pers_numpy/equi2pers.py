fix rotation axis

equilib/equi2pers/equi2pers_numpy/equi2pers.py
@@ -63,10 +63,18 @@ class Equi2Pers(BaseEqui2Pers):
         r"""Default rotation that changes global to camera coordinates
         """
         if not hasattr(self, '_g2c_rot'):
-            x = np.pi
-            y = np.pi
-            z = np.pi
-            self._g2c_rot = create_rotation_matrix(x=x, y=y, z=z)
+            R_XY = np.array([  # X <-> Y
+                [0., 1., 0.],
+                [1., 0., 0.],
+                [0., 0., 1.],
+                ])
+            R_YZ = np.array([  # Y <-> Z
+                [1., 0., 0.],
+                [0., 0., 1.],
+                [0., 1., 0.],
+            ])
+            self._g2c_rot = R_XY @ R_YZ
+
         return self._g2c_rot
 
     def rotation_matrix(
@@ -88,9 +96,7 @@ class Equi2Pers(BaseEqui2Pers):
         Camera coordinates -> z-axis points forward, y-axis points upward
         Global coordinates -> x-axis points forward, z-axis points upward
         """
-        R_g2c = self.global2camera_rotation_matrix
         R = create_rotation_matrix(x=roll, y=pitch, z=yaw)
-        R = R_g2c @ R
         return R
 
     @staticmethod
@@ -112,18 +118,16 @@ class Equi2Pers(BaseEqui2Pers):
         R = self.rotation_matrix(**rot)
 
         # conversion:
-        # m = P @ M
-        # P = K @ [R | t] = K @ R (in this case)
-        # M = R^-1 @ K^-1 @ m
         K_inv = np.linalg.inv(K)
-        R_inv = np.linalg.inv(R)
         m = m[:, :, :, np.newaxis]
-        M = R_inv @ K_inv @ m
+        M = R @ self._g2c_rot @ K_inv @ m
         M = M.squeeze(3)
 
         # calculate rotations per perspective coordinates
-        phi = np.arcsin(M[:, :, 1] / np.linalg.norm(M, axis=-1))
-        theta = np.arctan2(M[:, :, 0], M[:, :, 2])
+        # phi = np.arcsin(M[:, :, 1] / np.linalg.norm(M, axis=-1))
+        # theta = np.arctan2(M[:, :, 0], M[:, :, 2])
+        phi = np.arcsin(M[:, :, 2] / np.linalg.norm(M, axis=-1))
+        theta = np.arctan2(M[:, :, 1], M[:, :, 0])
 
         # center the image and convert to pixel location
         ui = (theta - np.pi) * w_equi / (2 * np.pi)

equilib/equi2pers/equi2pers_torch/equi2pers.py
@@ -70,10 +70,17 @@ class Equi2Pers(BaseEqui2Pers):
         r"""Default rotation that changes global to camera coordinates
         """
         if not hasattr(self, '_g2c_rot'):
-            x = math.pi
-            y = math.pi
-            z = math.pi
-            self._g2c_rot = create_rotation_matrix(x=x, y=y, z=z)
+            R_XY = torch.tensor([  # X <-> Y
+                [0., 1., 0.],
+                [1., 0., 0.],
+                [0., 0., 1.],
+                ])
+            R_YZ = torch.tensor([  # Y <-> Z
+                [1., 0., 0.],
+                [0., 0., 1.],
+                [0., 1., 0.],
+            ])
+            self._g2c_rot = R_XY @ R_YZ
         return self._g2c_rot
 
     def rotation_matrix(
@@ -95,9 +102,7 @@ class Equi2Pers(BaseEqui2Pers):
         Camera coordinates -> z-axis points forward, y-axis points upward
         Global coordinates -> x-axis points forward, z-axis poitns upward
         """
-        R_g2c = self.global2camera_rotation_matrix
         R = create_rotation_matrix(x=roll, y=pitch, z=yaw)
-        R = R_g2c @ R
         return R
 
     @staticmethod
@@ -154,15 +159,15 @@ class Equi2Pers(BaseEqui2Pers):
             m = self.perspective_coordinate
             K = self.intrinsic_matrix
             R = self.rotation_matrix(**r)
-            _M = R.inverse() @ K.inverse() @ m.unsqueeze(3)
+            _M = R @ self._g2c_rot @ K.inverse() @ m.unsqueeze(3)
             _M = _M.squeeze(3)
             M.append(_M)
         M = torch.stack(M, dim=0).to(device)
 
         # calculate rotations per perspective coordinates
         norms = torch.norm(M, dim=-1)
-        theta = torch.atan2(M[:, :, :, 0], M[:, :, :, 2])
-        phi = torch.asin(M[:, :, :, 1] / norms)
+        phi = torch.asin(M[:, :, :, 2] / norms)
+        theta = torch.atan2(M[:, :, :, 1], M[:, :, :, 0])
 
         # center the image and convert to pixel locatio
         ui = (theta - math.pi) * w_equi / (2 * math.pi)

scripts/demo_equi2equi_numpy.py
@@ -106,9 +106,9 @@ def test_image(path: str) -> None:
     """
     # Rotation:
     rot = {
-        'roll': 0,
-        'pitch': 0,
-        'yaw': 0,
+        'roll': 0,  #
+        'pitch': 0,  # vertical
+        'yaw': np.pi/4,  # horizontal
     }
 
     # Initialize equi2equi

scripts/demo_equi2equi_torch.py
@@ -129,9 +129,9 @@ def test_image(path: str) -> None:
     """
     # Rotation:
     rot = {
-        'roll': 0,
-        'pitch': 0,
-        'yaw': 0,
+        'roll': 0,  #
+        'pitch': np.pi/4,  # vertical
+        'yaw': np.pi/4,  # horizontal
     }
 
     # Initialize equi2equi