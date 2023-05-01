b1d1128120ab98c8ee5ef1f4961ce99a74b51055
haruishi43 <toraudonn@gmail.com>
2023-05-01 06:11:48 +0000

    update python versions

--- setup.py
+++ setup.py
@@ -108,9 +108,9 @@ setup(
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     python_requires=">=3.6",
 )
